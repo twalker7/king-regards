0a4174ce02803b5317320d0cec10af5b5bab9d15
Thomas Walker <thomasnwalker@yahoo.com>
2022-03-11 03:24:29 -0800

    direction change toward html poem completion

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import React, {useState, useRef}from 'react';
 import {Row, Col, Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import photo from  '../src/assets/ravi-kumar-unsplash.jpg'
-
 
 
 
@@ -22,50 +20,10 @@ function App() {
     };
     console.log(isFilled);
     setCount(count+ " hello");
-    function getArtwork(){
-      //Fill art box with "stand-by" gif for user's entertainment
-     // $("#painting").append('<iframe src="https://giphy.com/embed/LzCREPXRTqtdC" height="200" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/spongebob-LzCREPXRTqtdC"></a></p>');
-      //Fetch url for full list of art in API
-      fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects")
-      .then(response=>response.json())
-      .then(function(obj){
-          //For Testing:
-              //console.log(obj);
-          var artrandIndex = Math.floor(Math.random() * obj.objectIDs.length);
-          //alert("request test - return art ID:" + obj.objectIDs[artrandIndex]);
-          //Select random number based on array length
-          console.log("ID number: " , obj.objectIDs[artrandIndex]);
-          //Use random number to select artwork ID
-          var randomArtID = obj.objectIDs[artrandIndex];
-          localStorage.setItem("art-id", JSON.stringify(randomArtID));
-          getArtInfo(randomArtID);
-      });
-  }
-    
-
+  
 
   }
 
-  function getArtInfo(artID) {
-    //Use artwork ID to fetch url for all information about selected art
-    fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects/" + artID )
-    .then(response=>response.json())
-    .then(function(obj) {
-        //Find image link
-        var artImage = obj.primaryImage;
-        //For Testing:
-            //console.log("Image link: ", artImage);
-        //If statement to move ignore artwork with no image
-        if (artImage === "") {
-           // clearArt();
-           // getArtwork(); 
-        } else {
-            //clearArt();
-           // $("#painting").append('<img class="artwork-image" src="' + artImage +'"></img>'); 
-        }
-        
-    });
-}
    
   return (
       <div>
