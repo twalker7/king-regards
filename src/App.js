1451eb6abfd5dda01168796a8966cc96e5da0b0b
Thomas Walker <thomasnwalker@yahoo.com>
2022-03-11 11:58:52 -0800

    removed useless useState hook

--- src/App.js
+++ src/App.js
@@ -8,18 +8,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  const [isFilled, setIsFilled] = useState('../src/assets/ravi-kumar-unsplash.jpg');
-const sentence = "They can’t relate I feared debates with people who lack but are geared with hate I steered my fate and cleared my plate".split(" ");
+  const sentence = "They can’t relate I feared debates with people who lack but are geared with hate I steered my fate and cleared my plate".split(" ");
 
   const [count, setCount] = useState(null);
 
   
   function clickName(){
-    if(isFilled === '../src/assets/ravi-kumar-unsplash.jpg'){setIsFilled('../src/assets/ryan-wilson-unsplash.jpg');
-    }else{
-      setIsFilled('../src/assets/ravi-kumar-unsplash.jpg');
-    };
-    console.log(isFilled);
+
     setCount(count == null ? 0 : count + 1);
   
 
@@ -37,7 +32,7 @@ const sentence = "They can’t relate I feared debates with people who lack but 
         
   
           </div>
-          <Button className="explore-button" onClick={clickName}> Dig </Button>
+          <Button className="explore-button" onClick={clickName}> Proceed </Button>
         
          </div>
 
